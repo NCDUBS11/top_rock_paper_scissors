33d1eb0abb18a2539fe91ba6b151521737bade65
Nick <112791837+NCDUBS11@users.noreply.github.com>
2023-08-11 01:19:05 -0700

    Fix caseCorrection function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,6 +3,7 @@
 //Function to take player input and ensure only first letter is capitalized
 
 function caseCorrection(string){
+    if (string == ""){ return null; }
     let temp = string[0].toUpperCase() + string.slice(1).toLowerCase();
     return temp;
 }
@@ -32,41 +33,37 @@ function computerSelection(){
     case 1:
         console.log ("Computer chose: Rock");
         return "Rock";
-        break;
     case 2:
         console.log ("Computer chose: Paper");
         return "Paper";
-        break;
     case 3:
         console.log ("Computer chose: Scissors");
         return "Scissors";
-        break;
-    }
-        
+    }      
 }
 
 //Rock Paper Scissors Game function
 
-function rpsGame(playerSelection, computerSelection, playerScore, computerScore){
+function rpsGame(playerSelection, computerSelection){
 
     if (playerSelection == "Rock" && computerSelection == "Scissors"){
         alert("You Won! Rock beats Scissors!");
         console.log ("You won this round!");
-        return ++playerScore;
+        return true;
     }
 
     else if (playerSelection == "Paper" && computerSelection == "Rock"){
 
         alert("You Won! Paper beats Rock!");
         console.log ("You won this round!");
-        return ++playerScore;
+        return true;
     }
 
     else if (playerSelection == "Scissors" && computerSelection == "Paper"){
 
         alert("You Won! Scissors beats Paper!");
         console.log ("You won this round!");
-        return ++playerScore;
+        return true;
     }
 
     else if (playerSelection == computerSelection){
@@ -76,7 +73,7 @@ function rpsGame(playerSelection, computerSelection, playerScore, computerScore)
     }
     else{
         alert(`You Lost!  The computer beat your ${playerSelection} with ${computerSelection}!`);
-        return ++computerScore;
+        return false;
     }
 }
 
@@ -93,7 +90,8 @@ function rockPaperScissors(){
         alert (`Round ${round}!`);
         console.log (`Round ${round}!`);
 
-        rpsGame(playerSelection(), computerSelection())
+        rpsGame(playerSelection(), computerSelection());
+
         
         alert (`The current score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
         console.log (`The current score is: \nPlayer: ${playerScore} \nComputer: ${computerScore}`);
